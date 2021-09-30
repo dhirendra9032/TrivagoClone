033abb021537801360b1a4ef819a71cc667d9e93
Dhirendra Kumar <dhirendra9032@gmail.com>
2021-09-30 11:49:34 +0530

    initial commit

--- trivago_clone/src/App.js
+++ trivago_clone/src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
-import { Searchbar } from './Components/Searchbar/Searchbar';
+
 
 function App() {
   return (
     <div className="App">
-     <Searchbar/>
+    
     </div>
   );
 }
