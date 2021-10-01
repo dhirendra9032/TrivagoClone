19970a83d6405f2373943940171e0061fb012d25
piyushrana00456 <54103082+piyushrana00456@users.noreply.github.com>
2021-10-01 16:59:36 +0530

    built hotel page

--- trivago_clone/src/Pages/HomePage.jsx
+++ trivago_clone/src/Pages/HomePage.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Slider } from "../Components/Slider/Slider";
-// import Footer from "../Components/Footer/Footer";
-// import { Searchbar } from "../Components/Searchbar/Searchbar";
 import { RecentActivity } from "../Components/Recent/RecentActivity";
+import LandingPage from "../Components/LandingPage/LandingPage";
+import { Searchbar } from "../Components/Searchbar/Searchbar";
+import Footer from "../Components/Footer/Footer";
+import MainFooter from "../Components/Footer/MainFooter";
 // import { Link } from "react-router-dom";
 export const HomePage = () => {
   return (
     <div>
+      <LandingPage />
+      <Searchbar />
       <RecentActivity />
       <Slider />
+      <Footer />
+      <MainFooter />
     </div>
   );
 };
